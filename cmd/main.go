c83aa3771a11f3fd8727bd98c63f181d3a1e359e
ebezgodov <evgeniy.bezgodov@deiteriy.com>
2024-02-29 18:11:03 +0000

    Подготовил boilerplate.

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var i = 1
+
+func main() {
+	fmt.Printf("Hello, World! %d", i)
+	i++
+	fmt.Printf("Hello, World! %d", i)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ebezgodov/chat-server
+
+go 1.22.0
